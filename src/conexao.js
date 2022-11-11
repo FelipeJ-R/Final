da35bb4600eda6efacd8890953f894394af46490
FELIPE JESUS ROSARIO <felipejr.2021@outlook.com>
2022-11-10 22:09:57 -0300

    Ainda sem imagens

---
+++ src/conexao.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT
+});
+
+const query = (text, param) => {
+    return pool.query(text, param);
+}
+
+module.exports = {
+    query
+}
